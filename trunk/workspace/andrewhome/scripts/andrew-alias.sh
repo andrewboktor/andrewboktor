116c0bee8e978bdf2697ee67c16fde406e06386b
andrewboktor@gmail.com <andrewboktor@gmail.com@56b56de6-1a48-0410-9110-73031f465853>
2012-06-03 19:47:59 +0000

    Adding a whole bunch of scripts that should be used to configure my environment on any machine that has bash

---
+++ trunk/workspace/andrewhome/scripts/andrew-alias.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Andrew Boktor alias file
+
+export GREP_COLOR='1;33'
+alias grep='grep --color=auto'
+alias cgrep='grep --color=always'
+
+alias less='less -R'
+
+alias vi='vim'
+alias vim='vim -p'
+alias gvim='gvim -p'
+
+eval `dircolors -b`
+alias ls='ls --color=auto'
+alias ll='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias llh='ls -lh'
+alias llah='ls -lah'
+
