a975cc910b963495031aa9ac87a4248770f623ad
andrew.boktor@gmail.com <andrew.boktor@gmail.com@56b56de6-1a48-0410-9110-73031f465853>
2012-07-05 19:14:00 +0000

    Adding a quick but useful install script

---
+++ trunk/workspace/andrewhome/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ANDREWHOME_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+FILES=".bashrc .vimrc .vim .screenrc scripts"
+
+RAND=$RANDOM
+
+for f in $FILES
+do
+	if [ -e $HOME/$f ]; then
+		echo "$HOME/$f already exists, moving it to $HOME/$f.$RAND.backup"
+		mv $HOME/$f $HOME/$f.$RAND.backup
+	fi
+	ln -s $ANDREWHOME_ROOT/$f $HOME/$f
+	echo -n "Adding "
+	ls -l $HOME/$f
+done
