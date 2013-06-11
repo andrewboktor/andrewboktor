6eb60c0d155ced9b40e4fd6ee4d2b499103ff9f4
andrew.botros@gmail.com <andrew.botros@gmail.com@56b56de6-1a48-0410-9110-73031f465853>
2013-06-11 21:36:10 +0000

    cpufreq is no longer valid, may be later I will add other scripts

--- trunk/workspace/andrewhome/scripts/cpufreq/conservative.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-for i in 0 1 2 3; do sudo cpufreq-set -c $i -g conservative --max 2.1GHz; done
--- trunk/workspace/andrewhome/scripts/cpufreq/ondemand.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-for i in 0 1 2 3; do sudo cpufreq-set -c $i -g ondemand --max 2.1GHz; done
