77a2561b4c90aae1a01eaec4d00addacc5344ada
andrew.botros@gmail.com <andrew.botros@gmail.com@56b56de6-1a48-0410-9110-73031f465853>
2014-04-21 19:26:16 +0000

    Improving install.sh so that it doesn't mess with files if they were correctly setup adding ssh/config Adding ece.sh, still needs some changes

--- workspace/andrewhome/install.sh
+++ workspace/andrewhome/install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 ANDREWHOME_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-FILES=".bashrc .bash_profile .vimrc .vim .screenrc .rtorrent.rc scripts"
+FILES=".bashrc .bash_profile .vimrc .vim .screenrc .rtorrent.rc scripts .ssh/config"
 
 RAND=$RANDOM
 
 for f in $FILES
 do
-	if [ -e $HOME/$f ]; then
-		echo "$HOME/$f already exists, moving it to $HOME/$f.$RAND.backup"
-		mv $HOME/$f $HOME/$f.$RAND.backup
-	fi
-	ln -s $ANDREWHOME_ROOT/$f $HOME/$f
-	echo -n "Adding "
-	ls -l $HOME/$f
+    if [ -e $HOME/$f ]; then
+        f1=`realpath $HOME/$f`
+        f2=`realpath $ANDREWHOME_ROOT/$f`
+        if [ $f1 == $f2 ]; then
+            echo "$HOME/$f -> $ANDREWHOME_ROOT/$f is correctly linked. Not touching it."
+        else
+            echo "$HOME/$f already exists, moving it to $HOME/$f.$RAND.backup"
+            mv $HOME/$f $HOME/$f.$RAND.backup
+        fi
+    else
+        ln -s $ANDREWHOME_ROOT/$f $HOME/$f
+        echo -n "Adding "
+        ls -l $HOME/$f
+    fi
 done
